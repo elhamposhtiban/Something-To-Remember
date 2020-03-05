1ac65dd227fa38326eb0892139471d5e3378dd02
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-03-05 01:18:18 -0600

    testing application inorder to be sure that front-end and back-end is connected to each other

--- routes/api/dolist-api.js
+++ routes/api/dolist-api.js
@@ -1,21 +1,21 @@
-const router = require("express").Router();
-const dolistController = require ("../../controllers/dolistController");
+// const router = require("express").Router();
+// const dolistController = require ("../../controllers/dolistController");
 
 
-// Matches with "/api/dolist"
+// // Matches with "/api/dolist"
 
-router
-.route("/")
-.get(dolistController.findAll)
-.post(dolistController.create);
+// router
+// .route("/")
+// .get(dolistController.findAll)
+// .post(dolistController.create);
 
 
-// Matches with "/api/dolist/:id"
+// // Matches with "/api/dolist/:id"
 
-router
-.route("/:id")
-.get(dolistController.findById)
-.put(dolistController.update)
-.delete(dolistController.remove);
+// router
+// .route("/:id")
+// .get(dolistController.findById)
+// .put(dolistController.update)
+// .delete(dolistController.remove);
 
-module.exports = router
+// module.exports = router
