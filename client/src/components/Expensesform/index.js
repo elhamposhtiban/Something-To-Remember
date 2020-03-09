e69bd5b9313a3cf3d44144053304ad95fb220952
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-03-09 02:20:52 -0500

    adding hide and show function for expenses component add grid to scss file and working on styling expenses form

--- client/src/components/Expensesform/index.js
+++ client/src/components/Expensesform/index.js
@@ -1,24 +1,47 @@
 import React, {useState} from "react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const Expensesform = (
+const Expensesform = ()  => {
+
+    const [showExpenses, setShowExpenses] = useState(false)
 
 
-) => {
+   const showHandler = () => {
+     setShowExpenses(!showExpenses);
+   }
 
     return( 
-      
+
+        <div>
+            
+            <div className="row">
+            <div className= "col-1-of-2">
+                <button 
+                type="button"
+                className="btn btn-success mt-4 budget-manage-btn"
+                onClick= {showHandler}>
+                    add your budget
+                </button>
+            </div>
+            <div className= "col-2-of-2">
+                
+                <label className ="Dolist-label mt-4" htmlFor="budget"> Specify your total budget($)</label>
+                <input
+                    id="budget"
+                    name= "budget"
+                    type="number"
+                    className="form-control input-dolist"
+                    placeholder="budget"
+                />
+            </div>
+            </div>
+
+
+    {showExpenses &&  
       <form>
         <div className="form-group form-dolist">
-
-            <label className ="Dolist-label" htmlFor="budget"> Specify your total budget($)</label>
-            <input
-              id="budget"
-              name= "budget"
-              type="number"
-              className="form-control input-dolist"
-              placeholder="budget"
-            />
 
             <label className ="Dolist-label" htmlFor="item-name">Item Name</label>
             <input
@@ -74,12 +97,94 @@ const Expensesform = (
             </button>
       
         </div>
-    </form>
-    
-    )
+    </form> 
 }
+
+    </div>
+      
+      )
+    }
+
+    
+
 
 export default Expensesform;
 
 
 
+
+
+
+
+
+
+
+
+// class App extends React.Component {
+// 	state = {
+// 		shown: true,
+// 	};
+		
+// 	render() {		
+// 		return (
+// 			<div>
+// 				<h2>this.state.shown = {this.state.shown ? "true" : "false"}</h2>
+// 				<button onClick={() => this.setState({ shown: !this.state.shown })}>Toggle</button>
+// 			</div>
+// 		)
+// 	}
+// }
+
+
+
+
+
+
+
+
+// <section className="section-budget">
+// <div className="row">
+//     <div className="budget">
+//         <div className="budget__form">
+//             <form action="#" className="form">
+//                 <div className=" u-margin-bottom-medium">
+//                     <h2 className="heading-secondary">
+//                       Contact Info
+//                     </h2>
+//                 </div>
+//                 <div className="form__group input-group">
+//                   <div className="input-group-prepend">
+//                     {/* <span className="input-group-text">
+//                       <i className="form__icon fas fa-user fa-3x"></i>
+//                     </span> */}
+//                   </div>
+//                     <input type="text" placeholder="full name" id="name" className="form__input" required/>
+//                     <label for="name" className="form__label">Full name</label>
+//                 </div>
+
+//                 <div className="form__group input-group">
+//                   <div className="input-group-prepend">
+//                     <span className="input-group-text">
+//                       <i className=" form__icon far fa-envelope fa-3x"></i>
+//                     </span>
+//                   </div>
+//                     <input type="email" placeholder="Email address" id="email" className="form__input" required/>
+//                     <label for="email" className="form__label">Email address</label>
+//                 </div>
+//                 <div className="form__group input-group u-margin-bottom-medium">
+//                   <div className="input-group-prepend">
+//                     <span className=" input-group-text">
+//                     <i className="fas fa-comment fa-3x"></i>
+//                     </span>
+//                 </div>
+//                 <textarea type="text"  name="message" placeholder="Message" id="message-input" className="form__input" required></textarea>
+//                     <label for="message-input" className="form__label"> Message </label>
+//                 </div>
+//                 <div className="form__group">
+//                     <button className="btn btn--pink" type="submit" value="register">submit</button>
+//                 </div>
+//             </form>
+//           </div>
+//         </div>
+//     </div>
+// </section>
--- client/src/pages/expenses.js
+++ client/src/pages/expenses.js
@@ -1,4 +1,4 @@
-import React, {usestate} from "react";
+import React from "react";
 import Expensesform from "../components/Expensesform"
 
 
