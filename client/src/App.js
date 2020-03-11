0a46a798f4742a6d64619630944550f5017c8ec1
Makah <mecca-131@hotmail.com>
2020-03-11 02:07:11 -0500

    files

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
 
-import Dolist from "./components/pages/doList";
-import Budget from "./components/pages/expenses";
-import {Col,Row,Container} from "./components/Grid"
+// import Dolist from "./components/pages/doList";
+// import Budget from "./components/pages/expenses";
+// import {Col,Row,Container} from "./components/Grid"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -26,8 +26,8 @@ class App extends Component {
                 <div className="container">
                   <Route exact path="/register" component={ Register } />
                   <Route exact path="/login" component={ Login } />
-                  <Route exact path="/dolist" component={ Dolist } />
-                  <Route exact path="/budget" component={ Budget } />
+                  {/* <Route exact path="/dolist" component={ Dolist } />
+                  <Route exact path="/budget" component={ Budget } /> */}
                 </div>
                 </Switch>
             </div>
