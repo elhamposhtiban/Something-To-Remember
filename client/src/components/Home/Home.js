3b9912ac66ca792e536edd92fd0d403220f10f74
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-03-17 17:02:43 -0500

    working on style

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -8,11 +8,11 @@ const Home = () => {
 
         <section className="section-home">
       
-        <Carousel>
+        <Carousel className="carousel" >
             
-        <Carousel.Item>
+        <Carousel.Item className="carousel" >
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel__img"
             src="https://images.303magazine.com/uploads/2019/03/wedding-celebration.jpg"
             alt="First slide"
           />
@@ -20,14 +20,13 @@ const Home = () => {
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
+        </Carousel.Item >
+
+        <Carousel.Item className="carousel">
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel__img"
             src="https://s23527.pcdn.co/wp-content/uploads/2018/04/weddings-3225110_1920.jpg.optimal.jpg"
             alt="Third slide"
-       
-            
           />
       
           <Carousel.Caption>
@@ -35,9 +34,10 @@ const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel">
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel__img"
             src="https://www.brides.com/thmb/8Rvt7SaouAlrdp_L_4Es0feG4uE=/2400x2400/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__brides__proteus__5c1931e79abd092cf4bd393a__11-f2d4ff86cd6b4e1bb09ba0dab4d10068.jpeg"
             alt="Third slide"
           />
@@ -47,9 +47,10 @@ const Home = () => {
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item> 
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel">
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel__img"
             src="https://www.brides.com/thmb/oh7NFl5QEsxMKjKT7E5-IkI9-3I=/2048x1152/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__brides__proteus__5c1a91636b6bc209f4038244__169-1adbafcbb5c146aca5b1e969d9c1a961.jpeg"
             alt="Third slide"
           />
