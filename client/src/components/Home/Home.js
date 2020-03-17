76a31e3bdeb3e1a3ec718800540431366accd7be
Makah <mecca-131@hotmail.com>
2020-03-17 15:54:46 -0500

    slideshow for homepage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,64 @@
 import React from "react";
-
+import Carousel from 'react-bootstrap/Carousel'
 
 
 const Home = () => {
     return (
-        <section className="section-home">
-            <h1> this is a home page</h1>
-        </section>
+      
+        <Carousel>
+            
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.303magazine.com/uploads/2019/03/wedding-celebration.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://s23527.pcdn.co/wp-content/uploads/2018/04/weddings-3225110_1920.jpg.optimal.jpg"
+            alt="Third slide"
+       
+            
+          />
+      
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.brides.com/thmb/8Rvt7SaouAlrdp_L_4Es0feG4uE=/2400x2400/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__brides__proteus__5c1931e79abd092cf4bd393a__11-f2d4ff86cd6b4e1bb09ba0dab4d10068.jpeg"
+            alt="Third slide"
+          />
+      
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item> 
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.brides.com/thmb/oh7NFl5QEsxMKjKT7E5-IkI9-3I=/2048x1152/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__brides__proteus__5c1a91636b6bc209f4038244__169-1adbafcbb5c146aca5b1e969d9c1a961.jpeg"
+            alt="Third slide"
+          />
+      
+          <Carousel.Caption>
+            <h3>4th slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        
+      </Carousel>
     )
 }
 
